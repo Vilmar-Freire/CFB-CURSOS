9b69030d1e091f83977cd7318ec5f057caf812a4
Vilmar Freire <paipedroservino@gmail.com>
2025-09-23 23:16:53 -0300

    Fiz-mais-uma-alteração.

--- CFB-CURSOS-HTML5/java/index.js
+++
@@ -1,4 +0,0 @@
-var canvas=document.getElementById("desenho1");
-var ctx=canvas.getContext("2d");
-ctx.fillStyle="#00f";
-ctx.fillrect(0,0,200,200);
---
+++ CFB-CURSOS-HTML5/vilmar.js/index.js
@@ -0,0 +1,6 @@
+
+                        const quadro = document.querySelector("#quadro");
+                        const contexto = quadro.getContext("2d");
+                        contexto.fillStyle= '#blue';
+                        contexto.fillStyleRect(0, 0, 100 , 100);
+                        
